f8cd7c47cfb0b6f5141daaa63dcfa5eda4fc60e6
Golovkov Alex <6c65787870@gmail.com>
2018-01-26 01:33:13 +0300

    fix build

--- src/MicroElements.FileStorage/Abstractions/DeletedCollection.cs
+++ src/MicroElements.FileStorage/Abstractions/DeletedCollection.cs
@@ -1,3 +1,6 @@
+// Copyright (c) MicroElements. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 using System.Collections.Generic;
 using System.Linq;
 using ConcurrentCollections;
@@ -6,16 +9,18 @@ namespace MicroElements.FileStorage.Abstractions
 {
     public abstract class DeletedCollection
     {
-        protected readonly ConcurrentHashSet<string> KeysForDelete = new ConcurrentHashSet<string>();
-        
+        private readonly ConcurrentHashSet<string> _keysForDelete = new ConcurrentHashSet<string>();
+
+        protected ConcurrentHashSet<string> KeysForDelete => _keysForDelete;
+
         protected internal IEnumerable<string> GetDeletedKey()
         {
-            return KeysForDelete.AsEnumerable();
+            return _keysForDelete.AsEnumerable();
         }
 
         protected internal void DeleteKey(string key)
         {
-            KeysForDelete.TryRemove(key);
+            _keysForDelete.TryRemove(key);
         }
     }
 }

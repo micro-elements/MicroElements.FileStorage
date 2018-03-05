e36dac2570384e7accb08c0b7c1788c8262c69bc
Alexey Petriashev <petriashev@gmail.com>
2018-03-05 19:18:04 +0300

    FileAsync more close to corefx implementation

--- src/MicroElements.FileStorage/Utils/FileAsync.cs
+++ src/MicroElements.FileStorage/Utils/FileAsync.cs
@@ -1,7 +1,11 @@
 ﻿// Copyright (c) MicroElements. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Buffers;
 using System.IO;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using MicroElements.FileStorage.CodeContracts;
 
@@ -12,23 +16,153 @@ namespace MicroElements.FileStorage.Utils
     /// </summary>
     public class FileAsync
     {
-        public static async Task<string> ReadAllText(string path)
+        public static async Task<string> ReadAllText(string path, Encoding encoding)
         {
             Check.NotNull(path, nameof(path));
 
-            using (var reader = File.OpenText(path))
+            using (var reader = AsyncStreamReader(path, encoding))
             {
                 return await reader.ReadToEndAsync();
             }
         }
 
-        public static async Task WriteAllText(string path, string content)
+        public static async Task WriteAllText(string path, string content, Encoding encoding)
         {
             Check.NotNull(path, nameof(path));
 
-            using (var streamWriter = new StreamWriter(path, append: false))
+            using (var streamWriter = AsyncStreamWriter(path, encoding, append: false))
             {
                 await streamWriter.WriteAsync(content);
+            }
+        }
+
+        // see for current implementation: https://github.com/dotnet/corefx/blob/master/src/System.IO.FileSystem/src/System/IO/File.cs
+
+        internal const int DefaultBufferSize = 4096;
+        private static Encoding s_UTF8NoBOM;
+        // UTF-8 without BOM and with error detection. Same as the default encoding for StreamWriter.
+        private static Encoding UTF8NoBOM => s_UTF8NoBOM ?? (s_UTF8NoBOM = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true));
+
+
+        public static Task<string> ReadAllTextAsync(string path, CancellationToken cancellationToken = default(CancellationToken))
+            => ReadAllTextAsync(path, Encoding.UTF8, cancellationToken);
+
+        public static Task<string> ReadAllTextAsync(string path, Encoding encoding, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+            if (encoding == null)
+                throw new ArgumentNullException(nameof(encoding));
+            if (path.Length == 0)
+                throw new ArgumentException("Argument_EmptyPath", nameof(path));
+
+            return cancellationToken.IsCancellationRequested
+                ? Task.FromCanceled<string>(cancellationToken)
+                : InternalReadAllTextAsync(path, encoding, cancellationToken);
+        }
+
+        private static async Task<string> InternalReadAllTextAsync(string path, Encoding encoding, CancellationToken cancellationToken)
+        {
+            char[] buffer = null;
+            StreamReader sr = AsyncStreamReader(path, encoding);
+            try
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                buffer = ArrayPool<char>.Shared.Rent(sr.CurrentEncoding.GetMaxCharCount(DefaultBufferSize));
+                StringBuilder sb = new StringBuilder();
+                for (; ; )
+                {
+                    int read = await sr.ReadAsync(buffer, 0, DefaultBufferSize).ConfigureAwait(false);
+                    if (read == 0)
+                    {
+                        return sb.ToString();
+                    }
+
+                    sb.Append(buffer, 0, read);
+                }
+            }
+            finally
+            {
+                sr.Dispose();
+                if (buffer != null)
+                {
+                    ArrayPool<char>.Shared.Return(buffer);
+                }
+            }
+        }
+
+        public static Task WriteAllTextAsync(string path, string contents, CancellationToken cancellationToken = default(CancellationToken))
+            => WriteAllTextAsync(path, contents, UTF8NoBOM, cancellationToken);
+
+        public static Task WriteAllTextAsync(string path, string contents, Encoding encoding, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (path == null)
+                throw new ArgumentNullException(nameof(path));
+            if (encoding == null)
+                throw new ArgumentNullException(nameof(encoding));
+            if (path.Length == 0)
+                throw new ArgumentException("Argument_EmptyPath", nameof(path));
+
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.FromCanceled(cancellationToken);
+            }
+
+            if (string.IsNullOrEmpty(contents))
+            {
+                new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.Read).Dispose();
+                return Task.CompletedTask;
+            }
+
+            return InternalWriteAllTextAsync(AsyncStreamWriter(path, encoding, append: false), contents, cancellationToken);
+        }
+
+        // If we use the path-taking constructors we will not have FileOptions.Asynchronous set and
+        // we will have asynchronous file access faked by the thread pool. We want the real thing.
+        private static StreamReader AsyncStreamReader(string path, Encoding encoding)
+        {
+            FileStream stream = new FileStream(
+                path, FileMode.Open, FileAccess.Read, FileShare.Read, DefaultBufferSize,
+                FileOptions.Asynchronous | FileOptions.SequentialScan);
+
+            return new StreamReader(stream, encoding, detectEncodingFromByteOrderMarks: true);
+        }
+
+        private static StreamWriter AsyncStreamWriter(string path, Encoding encoding, bool append)
+        {
+            FileStream stream = new FileStream(
+                path, append ? FileMode.Append : FileMode.Create, FileAccess.Write, FileShare.Read, DefaultBufferSize,
+                FileOptions.Asynchronous | FileOptions.SequentialScan);
+
+            return new StreamWriter(stream, encoding);
+        }
+
+        private static async Task InternalWriteAllTextAsync(StreamWriter sw, string contents, CancellationToken cancellationToken)
+        {
+            char[] buffer = null;
+            try
+            {
+                buffer = ArrayPool<char>.Shared.Rent(DefaultBufferSize);
+                int count = contents.Length;
+                int index = 0;
+                while (index < count)
+                {
+                    int batchSize = Math.Min(DefaultBufferSize, count - index);
+                    contents.CopyTo(index, buffer, 0, batchSize);
+                    await sw.WriteAsync(buffer, 0, batchSize).ConfigureAwait(false);
+                    index += batchSize;
+                }
+
+                cancellationToken.ThrowIfCancellationRequested();
+                await sw.FlushAsync().ConfigureAwait(false);
+            }
+            finally
+            {
+                sw.Dispose();
+                if (buffer != null)
+                {
+                    ArrayPool<char>.Shared.Return(buffer);
+                }
             }
         }
     }
